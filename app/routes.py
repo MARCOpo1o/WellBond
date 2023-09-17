0612c6a93ddb3dd432f21e2291ab47aca4487fa4
MahmoudSalah02 <mahmouds343@gmail.com>
2023-09-16 22:27:33 -0400

    add patient details

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,6 @@ from app import app  # Import the app instance from the __init__.py file
 def index():
     return render_template('main_page.html')  # Use the correct template name
 
-# @app.route('/patients')
-# def dashboard():
-#     # Get data from DB (You may want to retrieve data from the database here)
-#     return render_template('main_page.html')  # Use the correct template name
+@app.route('/details')
+def details():
+    return render_template('patient_details.html')  # Use the correct template name
